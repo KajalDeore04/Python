59187c18cf3fb9fdc677b605ee779648ef7a2dd0
Kajal Deore <137670233+KajalDeore04@users.noreply.github.com>
2024-01-21 19:08:27 +0530

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# directly using input function
+
+print(input())          #prints what we provide
+x = input("number 1:")
+y = input("number 2:")
+print(x + y)            # every input we provide will be treated as string i.e. concatenate only
+print(int(x)+ int(y))   #typecasted and solved above problem
+

fed2e025ee36971f1500f4da5934454e0703e840
Kajal Deore <137670233+KajalDeore04@users.noreply.github.com>
2024-01-23 11:02:13 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,49 @@
+# performs tasks when called
+a = 9
+b = 8
+mean1 = (a*b)/(a+b)
+print(mean1)
+
+c = 10
+d = 15
+mean2 = (c*d)/(c+d)
+print(mean2)
+
+# to encapsulate the logic of above instead of writing separetly we use function
+
+def mean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+print("I am using function")
+mean(15,4)
+
+
+def perimeter(a,b):
+    pass                 # used when we want to write the body later so that it does not throw any error
+
+
+
+#Types of arguments
+# 1.Default
+def average(a=5, b=9):
+     print("Average is:", (a+b)/2)
+#
+# 2. Keyword
+average(b=9,a=5)   #sequence can be changed
+#
+# # 3. required
+def average(a, b = 9):
+    print("Average is:", (a+b)/2)   #a is required to run this function
+
+# 4. Variable length arguments (VarAgrs)
+def average(*numbers):                    # takes arguments as a tuple
+    for i in numbers:
+        sum = 0
+        sum = sum + i
+    print("Average is:", sum/len(numbers))     #will take as many as we provide
+
+
+average(5,4,6,8,9)
+
+# if used ** in argument then takes agrument as a dictionary

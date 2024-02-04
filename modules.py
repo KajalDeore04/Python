930146573a0ed9fbf27a9824c50d8c5e3a5fd3eb
Kajal Deore <137670233+KajalDeore04@users.noreply.github.com>
2024-02-04 20:04:14 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,18 @@
+# module - file containing python code . may contain function , classes
+# used with modular programming , which is to separate a program into parts
+
+#import basics #           -   now we can any function present in basics module
+
+#basics.hello()           - needs module name
+
+#import basics as base    - changed module name
+#base.hello()
+
+#from basics import hello  - imported function directly
+#hello()
+
+#from basics import *      - imported all the modules , nasty
+
+help("modules")   # prints list of prewritten modules  in python
+
+
